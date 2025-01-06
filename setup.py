f15d871de0332e8127bebd63486fa72ddeb9c2a1
rladeira <rwladeira@gmail.com>
2025-01-06 00:11:22 -0300

    Add additional information to the setup.py file.

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="bznames",
     version="0.0.1",
+    author="Rafael Ladeira",
+    description="Brazilian name generator using language models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rladeira/bznames",
     packages=setuptools.find_packages(),
     install_requires=[],
     extras_require={"dev": []},
